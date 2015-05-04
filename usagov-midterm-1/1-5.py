d5ee472d9ebbce52445e4380398b4f93e9fddbe7
ljforman <ljlandry@gmail.com>
2015-05-04 13:16:48 -0700

    My Midterm is Done.

--- usagov-midterm-1/1-5.py
+++ usagov-midterm-1/1-5.py
@@ -6,6 +6,6 @@ for name in names:
     atts = {'CountrySubdivision': name, 'NumberOfJobs': 1}
     resp = requests.get(BASE_USAJOBS_URL, params = atts)
     jobcount = resp.json()['TotalJobs']
-    thelist.append([name, jobcount])
+    thelist.append([name, int(jobcount)])
 
 print(thelist)
